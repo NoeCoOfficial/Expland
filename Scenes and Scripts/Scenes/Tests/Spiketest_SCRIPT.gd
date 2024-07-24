a4ec77ee5b6c8a4f6bf8a5fc1265dc47d620c581
sebashtioon <sebastiansuciu607@gmail.com>
2024-07-24 15:22:20 +1000

    OC28

---
+++ Scenes and Scripts/Scenes/Tests/Spiketest_SCRIPT.gd
@@ -0,0 +1,16 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		print("Player took damage.")
