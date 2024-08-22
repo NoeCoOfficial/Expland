1453a29e44bd40c61b8966dacabeb470bd966018
sebashtioon <sebastiansuciu607@gmail.com>
2024-08-22 13:45:56 +1000

    testing stuff

--- test_player_SCRIPT.gd
+++ test_player_SCRIPT.gd
@@ -1,5 +1,27 @@
 extends CharacterBody3D
 
+## SLOT MUST BE BETWEEN 1 AND 9
+## ITEM MUST BE A VALID ITEM (FOR EXAMPLE: "APPLE")
 func PopulateInventorySlot(Slot : int, Item : String):
 
-    ## This is where we define all of the items in the game.
+	## This is where we define all of the items in the game.
+
+	if Slot == 1:
+		pass
+    elif Slot == 2:
+        pass
+    elif Slot == 3:
+        pass
+    elif Slot == 4:
+        pass
+    elif Slot == 5:
+        pass
+    elif Slot == 6:
+        pass
+    elif Slot == 7:
+        pass
+    elif Slot == 8:
+        pass
+    elif Slot == 9:
+        pass
+
