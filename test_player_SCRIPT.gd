a2b300bd762f5576bfefaeefd35382bf7fd1d3d5
sebashtioon <sebastiansuciu607@gmail.com>
2024-08-22 13:58:13 +1000

    Still makin stuff

--- test_player_SCRIPT.gd
+++ test_player_SCRIPT.gd
@@ -6,22 +6,29 @@ func PopulateInventorySlot(Slot : int, Item : String):
 
 	## This is where we define all of the items in the game.
 
-	if Slot == 1:
+	if Slot == 1: # Check if the slot is 1
+
+		match Item:
+			"APPLE":
+				$PathToSlot_1.texture = InventoryManager.TEXTURE_APPLE # Define variables with the name of the item after "TEXTURE_"
+
+	elif Slot == 2: # Check if the slot is 1
+
+		match Item:
+			"APPLE":
+				$PathToSlot_1.texture = InventoryManager.TEXTURE_APPLE # Define variables with the name of the item after "TEXTURE_"
+	elif Slot == 3: # Check if the slot is 2
 		pass
-    elif Slot == 2:
-        pass
-    elif Slot == 3:
-        pass
-    elif Slot == 4:
-        pass
-    elif Slot == 5:
-        pass
-    elif Slot == 6:
-        pass
-    elif Slot == 7:
-        pass
-    elif Slot == 8:
-        pass
-    elif Slot == 9:
-        pass
+	elif Slot == 4: # Check if the slot is 3
+		pass
+	elif Slot == 5: # Check if the slot is 4
+		pass
+	elif Slot == 6: # Check if the slot is 5
+		pass
+	elif Slot == 7: # Check if the slot is 6
+		pass
+	elif Slot == 8: # Check if the slot is 7
+		pass
+	elif Slot == 9: # Check if the slot is 8
+		pass
 
