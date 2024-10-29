23a71a5b1a589e665fc789feb1a9e165f4393764
sebashtioon <sebastiansuciu607@gmail.com>
2024-10-29 13:30:34 +1100

    skibidi

--- Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
+++ Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
@@ -1,11 +1,14 @@
 extends Node
 
+var notification_spawned = false
 var is_colliding = false
 
 
-func spawn_interaction_notification():
-	var INTERACTION_LAYER = get_node("/root/World/Player/Head/Camera3D/InteractionLayer")
-	var NOTIFICATION = load("res://Scenes and Scripts/Scenes/Player/Interaction/InteractionHUD.tscn")
-	var NOTIFICATION_INSTANCE = NOTIFICATION.instantiate()
-	INTERACTION_LAYER.add_child(NOTIFICATION_INSTANCE)
-	NOTIFICATION_INSTANCE.position = Vector2(-228 ,0)
+func spawn_interaction_notification(_KEY:String, _MESSAGE:String):
+	if notification_spawned == false:
+		var INTERACTION_LAYER = get_node("/root/World/Player/Head/Camera3D/InteractionLayer")
+		var NOTIFICATION = load("res://Scenes and Scripts/Scenes/Player/Interaction/InteractionHUD.tscn")
+		var NOTIFICATION_INSTANCE = NOTIFICATION.instantiate()
+		INTERACTION_LAYER.add_child(NOTIFICATION_INSTANCE)
+		NOTIFICATION_INSTANCE.position = Vector2(-228 ,0)
+		notification_spawned = true
