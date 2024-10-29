3b455c98294d1a15b9cedaee85e0d5aa40bdd48a
sebashtioon <sebastiansuciu607@gmail.com>
2024-10-29 12:36:49 +1100

    add stuff to interaction manager

--- Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
+++ Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
@@ -1,3 +1,10 @@
 extends Node
 
 var is_colliding = false
+
+
+func spawn_interaction_notification():
+	var INTERACTION_LAYER = get_node("/root/World/Player/Head/Camera3D/InteractionLayer")
+	var NOTIFICATION = load("res://Scenes and Scripts/Scenes/Player/Interaction/InteractionHUD.tscn")
+	var NOTIFICATION_INSTANCE = NOTIFICATION.instantiate()
+	INTERACTION_LAYER.add_child(NOTIFICATION_INSTANCE)
