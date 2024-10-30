15a255f99c001e6a2a15a3c2b0540f663d367b2c
sebashtioon <sebastiansuciu607@gmail.com>
2024-10-30 15:47:54 +1100

    revamping system

--- Scenes and Scripts/Scenes/Player/Interaction/interactionHUD_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Player/Interaction/interactionHUD_SCRIPT.gd
@@ -1,19 +1,6 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
-
-
-func ShowLighterBG_NOTIFICATION():
+func ShowLighterBG_NOTIFICATION(KEY : String, MESSAGE : String):
 	var tween = get_tree().create_tween()
 	tween.tween_property($LighterBG, "position", Vector2(0, 30), 0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
 
--- Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
+++ Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
@@ -1,23 +1,6 @@
 extends Node
 
+
+
 var notification_spawned = false
 var is_colliding = false
-
-var NOTIFICATION
-var notification_contents
-var notification_lighterBG
-
-func spawn_interaction_notification(KEY : String, MESSAGE : String):
-	if notification_spawned == false:
-		NOTIFICATION = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD")
-
-		notification_lighterBG = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/LighterBG")
-		notification_contents = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/Contents")
-		
-		var NOTIFICATION_KEY = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/Contents/KEY")
-		var NOTIFICATION_MESSAGE = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/Contents/MESSAGE")
-
-		NOTIFICATION_KEY.text = KEY
-		NOTIFICATION_MESSAGE.text = MESSAGE
-
-		notification_spawned = true
