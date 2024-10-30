973bd1cca974a0d8d665aa377dbe8c4434df5470
sebashtioon <sebastiansuciu607@gmail.com>
2024-10-30 16:11:16 +1100

    refactor interaction HUD notifications and update raycast interaction

--- Scenes and Scripts/Scenes/Player/Interaction/Interaction_HUD_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Player/Interaction/Interaction_HUD_SCRIPT.gd
@@ -1,10 +1,12 @@
 extends Control
 
 
-
+func HideNotification():
+	print("<HIDE>")
 
 func ShowNotification(KEY : String, MESSAGE : String):
 	ShowLighterBG_NOTIFICATION()
+	ShowContents_NOTIFICATION(KEY, MESSAGE)
 
 func ShowLighterBG_NOTIFICATION():
 	var tween = get_tree().create_tween()
@@ -14,7 +16,7 @@ func ShowContents_NOTIFICATION(KEY : String, MESSAGE : String):
 	var tween = get_tree().create_tween()
 	$Contents/KEY.text = KEY
 	$Contents/MESSAGE.text = MESSAGE
-	tween.tween_property($Contents, "position", Vector2(0, 30), 0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.tween_property($Contents, "position:x", 200.0, 0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
 
 func HideLighterBG_NOTIFICATION():
 	var tween = get_tree().create_tween()
