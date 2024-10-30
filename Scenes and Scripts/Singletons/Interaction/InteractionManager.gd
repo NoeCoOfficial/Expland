75ee8f2b96f44c5363a74b132e75ab8c8662a82e
Sebastian Suciu <sebastiansuciu607@gmail.com>
2024-10-30 14:01:23 +1100

    add other vars

--- Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
+++ Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
@@ -13,5 +13,15 @@ func spawn_interaction_notification(_KEY:String, _MESSAGE:String):
 		NOTIFICATION_INSTANCE.position = Vector2(-228 , 30)
 		notification_spawned = true
 		var tween = get_tree().create_tween()
-		var notification_node = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD")
-		tween.tween_property(notification_node, "position:x", 0.0, 0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+
+
+		var notification_lighterBG = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/LighterBG")
+		var notification_contents = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/Contents")
+		
+		tween.tween_property(
+		notification_node, 
+		"position:x", 
+		0.0, 
+		0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+
+
