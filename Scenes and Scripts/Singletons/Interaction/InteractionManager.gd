04c2606fbb79b4f31fc581096aae4370c5af0d91
sebashtioon <sebastiansuciu607@gmail.com>
2024-10-30 15:01:18 +1100

    fix stuff

--- Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
+++ Scenes and Scripts/Singletons/Interaction/InteractionManager.gd
@@ -3,6 +3,7 @@ extends Node
 var notification_spawned = false
 var is_colliding = false
 
+var NOTIFICATION
 var notification_contents
 var notification_lighterBG
 
@@ -19,13 +20,13 @@ var notification_lighterBG
 
 func spawn_interaction_notification(KEY : String, MESSAGE : String):
 	if notification_spawned == false:
-		var NOTIFICATION = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD")
+		NOTIFICATION = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD")
 
 		notification_lighterBG = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/LighterBG")
 		notification_contents = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/Contents")
 		
-		var NOTIFICATION_KEY = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/KEY")
-		var NOTIFICATION_MESSAGE = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/MESSAGE")
+		var NOTIFICATION_KEY = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/Contents/KEY")
+		var NOTIFICATION_MESSAGE = get_node("/root/World/Player/Head/Camera3D/InteractionLayer/InteractionHUD/Contents/MESSAGE")
 
 		NOTIFICATION_KEY.text = KEY
 		NOTIFICATION_MESSAGE.text = MESSAGE
