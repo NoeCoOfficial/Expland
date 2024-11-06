e37197c33172ecf514892357801134b748b580e2
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-06 16:58:32 +1100

    Enhance debug output in ItemPickupObject_SCRIPT to include object path on successful load

--- Scenes and Scripts/Scenes/Player/Inventory/ItemPickupObject_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Player/Inventory/ItemPickupObject_SCRIPT.gd
@@ -29,7 +29,7 @@ func _ready():
 	if LOADED_OBJECT == null:
 		print("Failed to load object: ", PATH_TO_OBJECT)
 	else:
-		print("Object loaded successfully!")
+		print("Object loaded successfully! Path: " + PATH_TO_OBJECT)
 		
 		instantiated_object = LOADED_OBJECT.instantiate() # create an instance of the object
 		self.add_child(instantiated_object) # add it to the scene
