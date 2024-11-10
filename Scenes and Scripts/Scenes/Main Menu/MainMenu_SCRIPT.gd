9fbf327965d3a26e1122d29a714308d47123fe59
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-10 16:25:07 +1100

    Add Main Menu scene and script; remove old main_menu.tscn file

---
+++ Scenes and Scripts/Scenes/Main Menu/MainMenu_SCRIPT.gd
@@ -0,0 +1,12 @@
+@icon("res://Textures/Icons/Script Icons/32x32/main_menu.png")
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
