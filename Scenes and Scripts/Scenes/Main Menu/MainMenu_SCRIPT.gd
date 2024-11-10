a9e829c72cf266c21be6d8335c43b3b97b41bef9
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-11 06:51:53 +1100

    Add new UI elements to Main Menu and implement fade-out animation on startup

--- Scenes and Scripts/Scenes/Main Menu/MainMenu_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Main Menu/MainMenu_SCRIPT.gd
@@ -4,9 +4,23 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	await get_tree().create_timer(1).timeout
+	onStartup()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+
+func fadeOut():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Camera3D/MainLayer/FadeOut, "modulate", Color(0, 0, 0, 0), 5)
+
+func onStartup():
+	fadeOut()
+	var tween = get_tree().create_tween()
+	tween.tween_property($Camera3D/MainLayer/Logo, "position", Vector2(16, 24), 2).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO).set_delay(1)
+
+	
+	
