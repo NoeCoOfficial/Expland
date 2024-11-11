df81e32dc018801b82a1be37cd8e378d7abbf6d5
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-12 06:53:16 +1100

    Enhance Settings UI script to include volume sliders and update values dynamically

--- Scenes and Scripts/Scenes/Settings/SettingsUI_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Settings/SettingsUI_SCRIPT.gd
@@ -1,9 +1,13 @@
 extends Control
 
+@export var is_being_opened_by_a_button : bool
+
+
 func nodeSetup():
 	$SettingsTabContainer/Video/FOVSlider.value = PlayerSettingsData.FOV
-	
-
+	$SettingsTabContainer/Sound/MasterSlider.value = PlayerSettingsData.Master_Volume
+	$SettingsTabContainer/Sound/MusicSlider.value = PlayerSettingsData.music_Volume
+	$SettingsTabContainer/Sound/SFXSlider.value = PlayerSettingsData.sfx_Volume
 
 func _ready() -> void:
 	PlayerSettingsData.loadSettings()
@@ -12,7 +16,9 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	$SettingsTabContainer/Video/FOVValue.text = str(PlayerSettingsData.FOV)
-
+	$SettingsTabContainer/Sound/MasterValue.text = str(int(PlayerSettingsData.Master_Volume * 100))
+	$SettingsTabContainer/Sound/MusicValue.text = str(int(PlayerSettingsData.music_Volume * 100))
+	$SettingsTabContainer/Sound/SFXValue.text = str(int(PlayerSettingsData.sfx_Volume * 100))
 
 func _on_exit_settings_button_pressed() -> void:
 	pass
@@ -20,13 +26,41 @@ func _on_exit_settings_button_pressed() -> void:
 func openSettings():
 	pass
 
-
 func closeSettings():
 	pass
 
 
 func _on_fov_slider_value_changed(value: float) -> void:
 	PlayerSettingsData.FOV = value
+
+
+func _on_master_slider_value_changed(value: float) -> void:
+	PlayerSettingsData.Master_Volume = value
+
+func _on_music_slider_value_changed(value: float) -> void:
+	PlayerSettingsData.music_Volume = value
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	PlayerSettingsData.sfx_Volume = value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 func _on_save_settings_pressed() -> void:
