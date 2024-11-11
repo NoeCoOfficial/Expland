b1b9be6cb989509013d5491f3452d77ccd88bf11
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-12 06:47:04 +1100

    Fix FOV slider

--- Scenes and Scripts/Scenes/Settings/SettingsUI_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Settings/SettingsUI_SCRIPT.gd
@@ -1,12 +1,33 @@
 extends Control
 
+func nodeSetup():
+	$SettingsTabContainer/Video/FOVSlider.value = PlayerSettingsData.FOV
+	
+
+
 func _ready() -> void:
-	pass
+	PlayerSettingsData.loadSettings()
+	nodeSetup()
 
 
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	$SettingsTabContainer/Video/FOVValue.text = str(PlayerSettingsData.FOV)
 
 
 func _on_exit_settings_button_pressed() -> void:
 	pass
+
+func openSettings():
+	pass
+
+
+func closeSettings():
+	pass
+
+
+func _on_fov_slider_value_changed(value: float) -> void:
+	PlayerSettingsData.FOV = value
+
+
+func _on_save_settings_pressed() -> void:
+	PlayerSettingsData.saveSettings()
