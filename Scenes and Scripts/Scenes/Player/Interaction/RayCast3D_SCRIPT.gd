360eeccac49b82c328534945416621d150a4bc70
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-14 06:10:24 +1100

    Fix interaction bug (YAY)

--- Scenes and Scripts/Scenes/Player/Interaction/RayCast3D_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Player/Interaction/RayCast3D_SCRIPT.gd
@@ -40,15 +40,18 @@ var previous_collider = null
 
 func _physics_process(_delta: float) -> void:
 	if is_colliding():
-		InteractionManager.is_colliding = true
 		var collider = get_collider()
-		if collider and collider.has_method("on_raycast_hit"):
-			InteractionManager.spawn_interaction_notification("F", "Interact")
-			collider.on_raycast_hit()  # Calls the function on the hit body
-		previous_collider = collider
+		if collider != previous_collider:
+			if previous_collider and previous_collider.has_method("on_raycast_exit"):
+				previous_collider.on_raycast_exit()
+				InteractionManager.despawn_interaction_notification()
+			if collider and collider.has_method("on_raycast_hit"):
+				InteractionManager.spawn_interaction_notification("F", "Interact")
+				collider.on_raycast_hit()  # Calls the function on the hit body
+			previous_collider = collider
 	else:
-		InteractionManager.is_colliding = false
 		if previous_collider and previous_collider.has_method("on_raycast_exit"):
 			previous_collider.on_raycast_exit()
 			InteractionManager.despawn_interaction_notification()
 		previous_collider = null
+		InteractionManager.is_colliding = false
