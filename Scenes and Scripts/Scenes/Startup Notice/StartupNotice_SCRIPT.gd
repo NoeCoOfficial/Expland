93012391e838c6438f8b0016b6163db774833ae3
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-13 16:22:43 +1100

    Add debug notice to startup scene and hide in non-debug mode

--- Scenes and Scripts/Scenes/Startup Notice/StartupNotice_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Startup Notice/StartupNotice_SCRIPT.gd
@@ -2,9 +2,14 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if !OS.has_feature("debug"):
+		$Notice/DebugNotice.hide()
 	$Notice.text = "This is a pre-release version of Expland! 
 	You are currently playing v" + Utils.get_project_version() + "."
+	
+	$Notice/DebugNotice.self_modulate = Color(1, 1, 1, 0)
 	$Notice.self_modulate = Color(1, 1, 1, 0)
+	
 	await get_tree().create_timer(1).timeout
 	playTextFade()
 
@@ -15,11 +20,13 @@ func _process(_delta: float) -> void:
 func playTextFade():
 	var tween = get_tree().create_tween().set_parallel()
 	tween.tween_property($Notice, "self_modulate", Color(1, 1, 1, 1), 0.5)
+	tween.tween_property($Notice/DebugNotice, "self_modulate", Color(1, 1, 1, 1), 0.5)
 	tween.tween_property($Notice, "self_modulate", Color(1, 1, 1, 0), 0.5).set_delay(4)
+	tween.tween_property($Notice/DebugNotice, "self_modulate", Color(1, 1, 1, 0), 0.5).set_delay(4)
 	tween.connect("finished", Callable(self, "on_fade_finished"))
 
 func on_fade_finished():
 	call_deferred("change_to_main_menu")
 
-func change_to_main_menu() -> void:\
+func change_to_main_menu() -> void:
 	get_tree().change_scene_to_file("res://Scenes and Scripts/Scenes/Main Menu/MainMenu.tscn")
