8c261937de99ba76ffd8ecba08312a45ce602b09
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-21 12:48:16 +1100

    Add copyright notice and licensing information to TheIsland_SCRIPT.gd

--- Scenes and Scripts/Scenes/Player/Inventory/ItemPickupObject_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Player/Inventory/ItemPickupObject_SCRIPT.gd
@@ -45,7 +45,6 @@
 #                  noeco.official@gmail.com                     #
 # ============================================================= #
 
-
 @icon("res://Textures/Icons/Script Icons/32x32/item_drop.png")
 extends CharacterBody3D
 
