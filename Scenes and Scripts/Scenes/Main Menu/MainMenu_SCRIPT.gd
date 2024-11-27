f548794e2cb7992514d036fcd605ece47dba81ab
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-28 06:37:30 +1100

    Adjust button positions and offsets in Main Menu scene for improved layout

--- Scenes and Scripts/Scenes/Main Menu/MainMenu_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Main Menu/MainMenu_SCRIPT.gd
@@ -74,14 +74,6 @@ func _ready() -> void:
 	$Camera3D/MainLayer/FadeOut.visible = true
 	$Camera3D/MainLayer/GreyLayer.visible = true
 	
-	$Camera3D/MainLayer/PlayButton.position = Vector2(-348, 280)
-	$Camera3D/MainLayer/SettingsButton.position = Vector2(-348, 374)
-	$Camera3D/MainLayer/QuitButton.position = Vector2(-348, 470)
-	
-	$Camera3D/MainLayer/PlayButtonTrigger.position = Vector2(-348, 280)
-	$Camera3D/MainLayer/SettingsButtonTrigger.position = Vector2(-348, 374)
-	$Camera3D/MainLayer/QuitButtonTrigger.position = Vector2(-348, 470)
-	
 	await get_tree().create_timer(1).timeout
 	onStartup()
 
