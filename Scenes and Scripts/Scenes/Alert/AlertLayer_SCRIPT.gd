25ccf4368faecfd0f5623abc002b6287a38add91
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-30 10:54:57 +1100

    Update AlertLayer_SCRIPT: manage alert state with PauseManager for improved alert handling

--- Scenes and Scripts/Scenes/Alert/AlertLayer_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Alert/AlertLayer_SCRIPT.gd
@@ -55,6 +55,7 @@ func _ready() -> void:
 	$GreyLayer.modulate = Color(1, 1, 1, 0)
 
 func spawnAlert(title : String, text : String, textFontSize : int, animationTime : float):
+	PauseManager.is_inside_alert = true
 	$MainLayer/Title.text = title
 	$MainLayer/ScrollContainer/VBoxContainer/Text.text = text
 	$MainLayer/ScrollContainer/VBoxContainer/Text.add_theme_font_size_override("font_size", textFontSize)
@@ -66,6 +67,7 @@ func spawnAlert(title : String, text : String, textFontSize : int, animationTime
 	tween.tween_property($GreyLayer, "modulate", Color(1, 1, 1, 1), 0.3)
 
 func despawnAlert(animationTime : float):
+	PauseManager.is_inside_alert = false
 	var tween = get_tree().create_tween().set_parallel()
 	tween.tween_property($MainLayer, "scale", Vector2(0.0, 0.0), animationTime).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
 	tween.tween_property($GreyLayer, "modulate", Color(1, 1, 1, 0), 0.3)
