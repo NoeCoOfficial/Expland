64bd5a601b83dc481a31de5f02e3dee8d3673511
sebashtioon <sebastiansuciu607@gmail.com>
2024-11-29 16:56:47 +1100

    Add TextBG and ScrollContainer nodes to AlertLayer scene; implement spawnAlert function in script

--- Scenes and Scripts/Scenes/AlertLayer_SCRIPT.gd
+++ Scenes and Scripts/Scenes/AlertLayer_SCRIPT.gd
@@ -57,3 +57,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func spawnAlert(title : String, text : String, useScroll : bool, animationTime : float):
+	pass
