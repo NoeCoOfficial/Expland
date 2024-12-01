d4bf50a3330e44d075c4dcf6be48aa124c1f31ae
sebashtioon <sebastiansuciu607@gmail.com>
2024-12-01 18:26:10 +1100

    Add spawn button functionality to AchievementsUI and update tween properties for consistency

--- Scenes and Scripts/Scenes/Achievements/AchievementsUI_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Achievements/AchievementsUI_SCRIPT.gd
@@ -86,3 +86,7 @@ func _on_close_button_mouse_entered() -> void:
 
 func _on_close_button_pressed() -> void:
 	despawnAchievements(0.5)
+
+
+func _on_spawnbutton_pressed() -> void:
+	spawnAchievements(0.5)
