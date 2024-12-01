89e51c3c0c13dea0256578853ba8edd93e0ec38d
sebashtioon <sebastiansuciu607@gmail.com>
2024-12-01 18:22:09 +1100

    Update AlertLayer_SCRIPT to use animationTime for tween properties for consistency

--- Scenes and Scripts/Scenes/Alert/AlertLayer_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Alert/AlertLayer_SCRIPT.gd
@@ -70,15 +70,16 @@ func spawnAlert(title : String, text : String, textFontSize : int, animationTime
 	
 	var tween = get_tree().create_tween().set_parallel()
 	tween.tween_property($MainLayer, "scale", Vector2(1.0, 1.0), animationTime).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
-	tween.tween_property($GreyLayer, "modulate", Color(1, 1, 1, 1), 0.3)
+	tween.tween_property($GreyLayer, "modulate", Color(1, 1, 1, 1), animationTime)
 
 func despawnAlert(animationTime : float):
 	PauseManager.is_inside_alert = false
+	
 	var tween = get_tree().create_tween().set_parallel()
 	tween.tween_property($MainLayer, "scale", Vector2(0.0, 0.0), animationTime).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
-	tween.tween_property($GreyLayer, "modulate", Color(1, 1, 1, 0), 0.3)
+	tween.tween_property($GreyLayer, "modulate", Color(1, 1, 1, 0), animationTime)
 	
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(animationTime).timeout
 	
 	self.visible = false
 
