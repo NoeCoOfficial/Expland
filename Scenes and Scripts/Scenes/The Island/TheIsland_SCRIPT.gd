4158c9a573790f873fc62dfd6631007d6c562afe
sebashtioon <sebastiansuciu607@gmail.com>
2024-12-06 16:48:45 +1100

    Refactor item pickup integration and remove unused timer functionality

--- Scenes and Scripts/Scenes/The Island/TheIsland_SCRIPT.gd
+++ Scenes and Scripts/Scenes/The Island/TheIsland_SCRIPT.gd
@@ -70,6 +70,3 @@ func set_dof_blur(value : bool) -> void:
 		cameraAttributesResource.dof_blur_far_enabled = true
 	else:
 		cameraAttributesResource.dof_blur_far_enabled = false
-
-func on_test_timer_done() -> void:
-	InventoryManager.create_pickup_object_at_pos(Vector3(-351.588, 0, 324.606), "ROCK")
