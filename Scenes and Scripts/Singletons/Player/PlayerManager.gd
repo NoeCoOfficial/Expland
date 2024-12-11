ccd4876919eb0c25a891f578bb72de88de91bc2d
sebashtioon <sebastiansuciu607@gmail.com>
2024-12-12 09:27:39 +1100

    Add PlayerManager and SackclothBed finalized scripts; update ItemPickupObject name and project configuration

--- Scenes and Scripts/Singletons/Player/PlayerManager.gd
+++ Scenes and Scripts/Singletons/Player/PlayerManager.gd
@@ -1,5 +1,5 @@
 # ============================================================= #
-# ItemPickupObject_SCRIPT.gd
+# PlayerManager.gd [AUTOLOAD]
 # ============================================================= #
 #                       COPYRIGHT NOTICE                        #
 #                           Noe Co.                             #
@@ -45,84 +45,4 @@
 #                  noeco.official@gmail.com                     #
 # ============================================================= #
 
-@icon("res://Textures/Icons/Script Icons/32x32/item_drop.png")
-extends CharacterBody3D
-
-var gravity = 12.0
-var is_picked_up = false  # Flag to indicate if the item has been picked up
-
-@export var ITEM_TYPE: String:
-	set(value):
-		ITEM_TYPE = value
-		PATH_TO_OBJECT = "res://Assets/3D Models/Inventory Pickup Items/" + ITEM_TYPE + ".blend"
-	get:
-		return ITEM_TYPE
-
-var PATH_TO_OBJECT: String
-var instantiated_object # Variable to hold the reference to the instantiated object
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	move_and_slide()
-
-func _ready():
-	self.name = "ItemPickupObject"
-	self.scale = Vector3(0.0001, 0.0001, 0.0001)
-	
-	if InventoryManager.creatingFromInventory:
-		self.global_position = get_node("/root/World/Player/Head/ItemDropPosition").global_position
-	
-	var tween = get_tree().create_tween()
-	
-	if InventoryManager.creatingFromInventory:
-		ITEM_TYPE = InventoryManager.item_ref
-	else:
-		ITEM_TYPE = InventoryManager.item_ref_not_at_inventory
-	
-	var LOADED_OBJECT = load(PATH_TO_OBJECT)
-	
-	# null checks
-	if LOADED_OBJECT == null:
-		print("Failed to load object: ", PATH_TO_OBJECT)
-	else:
-		print("{LOCAL} [ItemPickupObject_SCRIPT.gd] Object loaded successfully! Path: " + PATH_TO_OBJECT)
-		
-		instantiated_object = LOADED_OBJECT.instantiate() # Create an instance of the object
-		self.add_child(instantiated_object) # Add it to the scene
-		PlayBobbingAnimation()
-		PlaySpinningAnimation()
-		
-		tween.tween_property(self, "scale", Vector3(0.2, 0.2, 0.2), 0.2).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
-
-func PlaySpinningAnimation():
-	var tween = get_tree().create_tween()
-	var current_rotation_y = instantiated_object.rotation.y
-	var final_rotation_y = current_rotation_y + 2 * PI  # Rotate 360 degrees
-	tween.tween_property(instantiated_object, "rotation:y", final_rotation_y, 3)
-	tween.connect("finished", Callable(self, "_on_spinning_anim_finished"))
-
-func _on_spinning_anim_finished():
-	# Reset rotation to avoid floating-point precision issues
-	instantiated_object.rotation.y = fmod(instantiated_object.rotation.y, 2 * PI)
-	PlaySpinningAnimation()
-
-func PlayBobbingAnimation():
-	var tween = get_tree().create_tween()
-	var default_y = instantiated_object.position.y
-	var final_y_top = default_y + 1.5
-	var time = 1.3
-	tween.tween_property(instantiated_object, "position:y", final_y_top, time).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
-	tween.tween_property(instantiated_object, "position:y", default_y, time).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
-	tween.connect("finished", Callable(self, "_on_bobbing_anim_finished"))
-
-func _on_bobbing_anim_finished():
-	PlayBobbingAnimation()
-
-func get_ITEM_TYPE():
-	return ITEM_TYPE
-
-func mark_as_picked_up():
-	is_picked_up = true
+extends Node
