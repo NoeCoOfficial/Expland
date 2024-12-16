1decb156e5af4c8c5c55e077024fd65b66c8bf65
sebashtioon <sebastiansuciu607@gmail.com>
2024-12-16 14:39:06 +1100

    refactor: update chest transform and add animation finished signal handling

--- Assets/3D Models/Interactive/Finalized/Chest/Chest_Finalized_StaticBody3D_SCRIPT.gd
+++ Assets/3D Models/Interactive/Finalized/Chest/Chest_Finalized_StaticBody3D_SCRIPT.gd
@@ -47,12 +47,13 @@
 
 extends StaticBody3D
 
+var ANIMATING = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _on_anim_finished(anim_name: StringName) -> void:
+	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func is_animating():
+	return ANIMATING
