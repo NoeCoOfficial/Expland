cff7c54b319e5f6fb629775cd24a3bd0bfea9241
sebashtioon <sebastiansuciu607@gmail.com>
2024-12-20 08:32:55 +1100

    chore: update version to v0.5.5 in Main Menu and Startup Notice; adjust wait time in The Island scene; add empty file check in PlayerData

--- Scenes and Scripts/Singletons/Save and Load/PlayerData.gd
+++ Scenes and Scripts/Singletons/Save and Load/PlayerData.gd
@@ -84,7 +84,13 @@ func loadData(withOutput : bool) -> void:
 		return
 	
 	if file and not file.eof_reached():
-		var current_line = JSON.parse_string(file.get_line())
+		var line = file.get_line()
+		if line.empty():
+			push_warning("[PlayerData] File is empty (" + SAVE_PATH + ")")
+			file.close()
+			return
+		
+		var current_line = JSON.parse_string(line)
 		if current_line:
 			TimeManager.CURRENT_HOUR = current_line["CURRENT_HOUR"]
 			TimeManager.CURRENT_DAY = current_line["CURRENT_DAY"]
