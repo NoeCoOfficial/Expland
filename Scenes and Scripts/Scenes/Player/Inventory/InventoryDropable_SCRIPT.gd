e0f77e3026b546b4b516624b2005ac247271f28e
sebashtioon <sebastiansuciu607@gmail.com>
2024-12-23 14:36:44 +1100

    refactor: Remove unnecessary whitespace in InventoryDropable_SCRIPT

--- Scenes and Scripts/Scenes/Player/Inventory/InventoryDropable_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Player/Inventory/InventoryDropable_SCRIPT.gd
@@ -175,7 +175,6 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("dropable"):
 		is_inside_dropable = false
-		
 
 func _on_area_2d_mouse_entered():
 	if not InventoryManager.is_dragging:
