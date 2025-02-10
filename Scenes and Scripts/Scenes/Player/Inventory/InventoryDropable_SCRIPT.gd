8fc8ae85f54d756f1bb1396c7085836c8f60ef96
sebashtioon <sebastiansuciu607@gmail.com>
2025-02-10 19:01:57 +1100

    refactor: simplify position handling in InventoryDropable_SCRIPT, optimize

--- Scenes and Scripts/Scenes/Player/Inventory/InventoryDropable_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Player/Inventory/InventoryDropable_SCRIPT.gd
@@ -162,11 +162,10 @@ func _process(delta):
 				
 				InventoryManager.is_dragging = false
 				self.z_index = 0
-				var tween = get_tree().create_tween()
 				if is_inside_dropable and !InventoryManager.is_inside_checker:
 					if body_ref == slot_inside:
 						# If the draggable is placed back into its current slot, do nothing
-						tween.tween_property(self, "global_position", body_ref.global_position, SNAP_TIME)
+						global_position = body_ref.global_position
 						
 						if is_in_chest_slot:
 							is_in_chest_slot = true
@@ -189,7 +188,7 @@ func _process(delta):
 					else:
 						if body_ref.has_method("is_populated") and body_ref.is_populated():
 							# If the slot is already populated, snap back to the original position
-							tween.tween_property(self, "global_position", initialPos, SNAP_TIME)
+							global_position = initialPos
 							
 							if is_in_chest_slot:
 								is_in_chest_slot = true
@@ -211,7 +210,7 @@ func _process(delta):
 							
 						else:
 							# If the slot is not populated, snap to the new slot
-							tween.tween_property(self, "global_position", body_ref.global_position, SNAP_TIME)
+							global_position = body_ref.global_position
 							if body_ref.has_method("set_populated"):
 								body_ref.set_populated(true)
 								
@@ -273,7 +272,7 @@ func _process(delta):
 							else:
 								print("{LOCAL} [InventoryDropable_SCRIPT.gd] " + body_ref + " does not have method: set_populated()")
 				else:
-					tween.tween_property(self, "global_position", initialPos, SNAP_TIME)
+					global_position = initialPos
 					
 					if is_in_chest_slot:
 						is_in_chest_slot = true
