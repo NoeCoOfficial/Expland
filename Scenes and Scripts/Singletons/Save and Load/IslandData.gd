82370615c49301bfa4b341031a99a93fdb940aa4
sebashtioon <sebastiansuciu607@gmail.com>
2025-02-12 15:50:17 +1100

    feat: add version information to saved data in IslandData.gd

--- Scenes and Scripts/Singletons/Save and Load/IslandData.gd
+++ Scenes and Scripts/Singletons/Save and Load/IslandData.gd
@@ -66,7 +66,8 @@ func saveData(Island_Name : String) -> void:
 	
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
 	var data = {
-		"Collected_Explorer_Notes" : ExplorerNotesManager.COLLECTED_NOTES
+		"Version" : Global.VERSION,
+		"Collected_Explorer_Notes" : ExplorerNotesManager.COLLECTED_NOTES,
 	}
 	
 	var jstr = JSON.stringify(data)
@@ -97,12 +98,12 @@ func loadData(Island_Name : String, withOutput : bool) -> void:
 		var current_line = JSON.parse_string(file.get_line())
 		
 		if current_line:
+			
 			ExplorerNotesManager.COLLECTED_NOTES = current_line["Collected_Explorer_Notes"]
 			
-				
 			if withOutput:
 				print_rich("[center][font=res://Fonts/CabinetGrotesk/CabinetGrotesk-Black.otf][font_size=30]-- ISLAND DATA HAS BEEN LOADED --[/font_size][/font][/center]")
 				print_rich("[center][font_size=15][font=res://Fonts/CabinetGrotesk/CabinetGrotesk-Bold.otf]Collected explorer notes: " + str(ExplorerNotesManager.COLLECTED_NOTES) + "[/font][/font_size][/center]")
-			
+		
 		file.close()
 		
