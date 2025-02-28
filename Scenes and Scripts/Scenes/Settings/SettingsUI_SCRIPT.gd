e7ca6fd62ff48981574972537251a1475b208da1
sebashtioon <sebastiansuciu607@gmail.com>
2025-03-01 07:23:09 +1100

    feat: add Quick Animations toggle to settings UI

--- Scenes and Scripts/Scenes/Settings/SettingsUI_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Settings/SettingsUI_SCRIPT.gd
@@ -53,6 +53,7 @@ func nodeSetup():
 	
 	$GreyLayer.modulate = Color(1, 1, 1, 0)
 	
+	$MainLayer/SettingsTabContainer/General/QASwitch.button_pressed = PlayerSettingsData.quickAnimations
 	$MainLayer/SettingsTabContainer/Graphics/PrettyShadowsSwitch.button_pressed = PlayerSettingsData.PrettyShadows
 	$MainLayer/SettingsTabContainer/General/SSCSwitch.button_pressed = PlayerSettingsData.showStartupScreen
 	$MainLayer/SettingsTabContainer/General/AutosaveIntervalSpinBox.value = PlayerSettingsData.autosaveInterval
@@ -165,3 +166,9 @@ func _on_ssc_switch_toggled(toggled_on: bool) -> void:
 func _on_autosave_interval_spin_box_value_changed(value: float) -> void:
 	PlayerSettingsData.autosaveInterval = int(value)
 	PlayerSettingsData.set_autosave_interval(int(value))
+
+func _on_qa_switch_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		PlayerSettingsData.quickAnimations = true
+	else:
+		PlayerSettingsData.quickAnimations = false
