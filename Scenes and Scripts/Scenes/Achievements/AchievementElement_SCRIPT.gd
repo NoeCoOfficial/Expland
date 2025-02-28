f19726ba80b9a6638a0598d6a844680ca69d1658
Sebastian Suciu <sebastiansuciu607@gmail.com>
2025-02-28 11:32:41 +1100

    add: new time parameter

--- Scenes and Scripts/Scenes/Achievements/AchievementElement_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Achievements/AchievementElement_SCRIPT.gd
@@ -47,9 +47,9 @@
 
 extends Control
 
-func _update(e_name: String, e_image_path: String, e_description: String):
+func _update(e_name: String, e_image_path: String, e_description: String, e_time_str : String):
     var texture = load(e_image_path)  # Load the image as a Texture2D
-	
+
     if texture is Texture2D:  # Ensure it's a valid texture
         $Elements/e_image_container/e_image.texture = texture
     else:
