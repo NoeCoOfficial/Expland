88be17f3e4616ed18eb8939659c40019a179795d
Sebastian Suciu <sebastiansuciu607@gmail.com>
2025-02-28 03:02:02 +0000

    refactor: simplify visibility logic for ProtectiveLayer based on quick animations setting

--- Scenes and Scripts/Scenes/Main Menu/MainMenu_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Main Menu/MainMenu_SCRIPT.gd
@@ -116,9 +116,11 @@ func _ready() -> void:
 	await get_tree().create_timer(1).timeout
 	onStartup()
 	
-	#if !PlayerSettingsData.quickAnimations:
-	await get_tree().create_timer(2).timeout
-	$Camera3D/MainLayer/ProtectiveLayer.visible = false
+	if PlayerSettingsData.quickAnimations:
+		$Camera3D/MainLayer/ProtectiveLayer.visible = false
+	else:
+		await get_tree().create_timer(2).timeout
+		$Camera3D/MainLayer/ProtectiveLayer.visible = false
 
 func change_to_startup_notice() -> void:
 	get_tree().change_scene_to_packed(StartupNotice)
