de162b058a24dd19eb0110eea4eec02a52e58ec2
sebashtioon <sebastiansuciu607@gmail.com>
2025-03-10 11:37:09 +1100

    feat: set player instance position for improved scene alignment in Start Cutscene

--- Scenes and Scripts/Scenes/Story Mode/Start Cutscene/StoryMode_StartCutscene_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Story Mode/Start Cutscene/StoryMode_StartCutscene_SCRIPT.gd
@@ -82,6 +82,7 @@ func onfadeInGreyOverlay_Finished():
 	$Camera_Sutle_Movement.stop()
 	$"Yacht Bob".stop()
 	self.add_child(player_instance)
+	player_instance.position = Vector3(2.07, -15.328, -17.33)
 	player_instance.nodeSetup()
 	$Camera3D.clear_current(true)
 	$Camera3D/MainLayer/BlackFade.hide()
