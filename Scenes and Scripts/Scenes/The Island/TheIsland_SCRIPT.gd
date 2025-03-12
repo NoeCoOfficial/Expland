ad0bb12e474914c49916ad557e32ac67f84fb44d
sebashtioon <sebastiansuciu607@gmail.com>
2025-03-12 15:43:51 +1100

    feat: update sky and ground colors for improved aesthetics and enhance weather transition logic

--- Scenes and Scripts/Scenes/The Island/TheIsland_SCRIPT.gd
+++ Scenes and Scripts/Scenes/The Island/TheIsland_SCRIPT.gd
@@ -184,7 +184,21 @@ func append_random_songs(song_array: Array):
 
 func change_sky(SkyType: String, TOD : int):
 	if transitioning_weather:
+		print("Already transitioning sky. Can't preform desired operation.")
 		return
+	
+	if SkyType == "CLOUDY":
+		
+		transitioning_weather = true
+		
+		DayNightCycle_Sky.play(&"sky_cycle")
+		DayNightCycle_Sky.seek(TOD * 2)
+		
+		DayNightCycle.play(&"cycle")
+		DayNightCycle.seek(TOD * 2)
+		
+		await get_tree().create_timer(30.0).timeout
+		transitioning_weather = false
 	
 	if SkyType == "CLOUDY":
 		
@@ -215,16 +229,21 @@ func change_weather(GOTO_WEATHER_STR : String, ARR_INDEX : int):
 	print_rich("[color=pink]Changing weather to: " + GOTO_WEATHER_STR + "[/color]")
 	
 	if GOTO_WEATHER_STR == "SUNNY":
-		# Implement SUNNY weather change logic
-		pass
+		var tween = get_tree().create_tween()
+		tween.connect("finished", _on_rain_color_fade_out_finished)
+		tween.tween_property($Rain, "color", Color(1, 1, 1, 0), 30.0)
+		change_sky("SUNNY", TimeManager.CURRENT_TIME)
 	
 	elif GOTO_WEATHER_STR == "CLOUDY":
-		# Implement CLOUDY weather change logic
-		pass
+		var tween = get_tree().create_tween()
+		tween.connect("finished", _on_rain_color_fade_out_finished)
+		tween.tween_property($Rain, "color", Color(1, 1, 1, 0), 30.0)
+		change_sky("LIGHT_RAIN", TimeManager.CURRENT_TIME)
 	
 	elif GOTO_WEATHER_STR == "RAIN":
 		$Rain.emitting = true
-		$Rain.amount = 5000
+		if !WeatherManager.CURRENT_WEATHER == "RAIN" and !WeatherManager.CURRENT_WEATHER == "STORM" and !WeatherManager.CURRENT_WEATHER == "LIGHT_RAIN":
+			$Rain.amount = 5000
 		$Rain.visible = true
 		var tween = get_tree().create_tween()
 		tween.tween_property($Rain, "color", Color(1, 1, 1, 1), 30.0).from(Color(1, 1, 1, 0))
@@ -233,7 +252,7 @@ func change_weather(GOTO_WEATHER_STR : String, ARR_INDEX : int):
 	elif GOTO_WEATHER_STR == "LIGHT_RAIN":
 		$Rain.emitting = true
 		
-		if !WeatherManager.CURRENT_WEATHER == "RAIN" and !WeatherManager.CURRENT_WEATHER == "STORM":
+		if !WeatherManager.CURRENT_WEATHER == "RAIN" and !WeatherManager.CURRENT_WEATHER == "STORM" and !WeatherManager.CURRENT_WEATHER == "LIGHT_RAIN":
 			$Rain.amount = 2500
 		
 		$Rain.visible = true
@@ -247,6 +266,9 @@ func change_weather(GOTO_WEATHER_STR : String, ARR_INDEX : int):
 	
 	WeatherManager.CURRENT_WEATHER = WeatherManager.WEATHERS[ARR_INDEX]
 	WeatherManager.CURRENT_WEATHER_ARR_INDEX = ARR_INDEX
+
+func _on_rain_color_fade_out_finished():
+	$Rain.emitting = false
 
 func _on_weather_timer_timeout() -> void:
 	init_weather()
