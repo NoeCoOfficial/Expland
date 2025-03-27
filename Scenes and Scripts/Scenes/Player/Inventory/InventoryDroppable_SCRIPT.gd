23ed8f839ed30837a301336058f67456d2a6e1c3
sebashtioon <sebastiansuciu607@gmail.com>
2025-03-27 14:17:38 +1100

    feat: add default size property and improve droppable scaling logic during drag events

--- Scenes and Scripts/Scenes/Player/Inventory/InventoryDroppable_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Player/Inventory/InventoryDroppable_SCRIPT.gd
@@ -64,6 +64,7 @@ extends Node2D
 @export var Stack_Count : int = 1
 @export var Mouse_Inside_Droppable : bool = false
 @export var Debounce_Timer_0 : bool = false
+@export var Default_Size : Vector2 = Vector2(1.0, 1.0)
 
 @export_subgroup("Slot Related")
 @export var Populating_Slot_Node : Node
@@ -111,15 +112,21 @@ func _on_mouse_detector_mouse_shape_entered(shape_idx: int) -> void:
 	Mouse_Inside_Droppable = true
 	
 	# Slightly scale up the droppable for a nice effect
-	if !InventoryManager.is_dragging:
-		scale = Vector2(scale.x * 1.05, scale.y * 1.05)
+	if InventoryManager.is_dragging:
+		if InventoryManager.currently_dragging_node == self:
+			scale = Vector2(Default_Size.x * 1.05, Default_Size.y * 1.05)
+	else:
+		scale = Vector2(Default_Size.x * 1.05, Default_Size.y * 1.05)
 
 func _on_mouse_detector_mouse_shape_exited(shape_idx: int) -> void:
 	Mouse_Inside_Droppable = false
 	
 	# Scale down the droppable to the original size
-	if !InventoryManager.is_dragging:	
-		scale = Vector2(scale.x / 1.05, scale.y / 1.05)
+	if InventoryManager.is_dragging:
+		if InventoryManager.currently_dragging_node == self:
+			scale = Vector2(Default_Size.x / 1.05, Default_Size.y / 1.05)
+	else:
+		scale = Vector2(Default_Size.x / 1.05, Default_Size.y / 1.05)
 
 
 func _on_droppable_mouse_detector_mouse_entered() -> void:
