cde6c24b2266ce8add9aeda308e9c0f4fa0c4700
sebashtioon <sebastiansuciu607@gmail.com>
2025-04-05 20:23:18 +1100

    Add AXE hand item resource and update inventory manager

--- Scenes and Scripts/Singletons/Inventory/InventoryManager.gd
+++ Scenes and Scripts/Singletons/Inventory/InventoryManager.gd
@@ -96,6 +96,7 @@ const ITEM_TYPES = {
 		"DESCRIPTION" : "A tool used for cutting down trees.",
 		"IMAGE_LOAD" : preload("uid://dq8jg5iy6tfyc"),
 		"PICKUP_LOAD" : preload("uid://mmat24b7ftxq"),
+		"HAND_ITEM_RES" : preload("uid://bpa3rcaw6dav2"),
 		"MAX_STACK" : 1,
 	},
 	
