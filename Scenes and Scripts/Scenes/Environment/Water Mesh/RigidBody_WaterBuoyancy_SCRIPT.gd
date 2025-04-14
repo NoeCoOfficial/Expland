9f728e001301bf1033d213245a395947e1820eea
sebashtioon <sebastiansuciu607@gmail.com>
2025-04-14 23:30:30 +1000

    Add buoyancy calculations and drag effects to RigidBody_WaterBuoyancy script

--- Scenes and Scripts/Scenes/Environment/Water Mesh/RigidBody_WaterBuoyancy_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Environment/Water Mesh/RigidBody_WaterBuoyancy_SCRIPT.gd
@@ -46,3 +46,28 @@
 # ============================================================= #
 
 extends RigidBody3D
+
+@export var float_force := 1.0
+@export var water_drag := 0.05
+@export var water_angular_drag := 0.05
+
+@export var gravity : float = 9.8
+@export var water_mesh : MeshInstance3D
+@export var probe_container : Node3D
+
+@onready var probes = probe_container.get_children()
+
+var submerged : bool = false
+
+func _physics_process(delta):
+	submerged = false
+	for p in probes:
+		var depth = water_mesh.get_height(p.global_position) - p.global_position.y 
+		if depth > 0:
+			submerged = true
+			apply_force(Vector3.UP * float_force * gravity * depth, p.global_position - global_position)
+
+func _integrate_forces(state: PhysicsDirectBodyState3D):
+	if submerged:
+		state.linear_velocity *=  1 - water_drag
+		state.angular_velocity *= 1 - water_angular_drag 
