741448ed0bf283abffc4ab938c6f80e4779e5c1c
sebashtioon <sebastiansuciu607@gmail.com>
2025-04-22 19:18:45 +1000

    Update Player collision shape handling in BuildingManager: set global transform for CanBuildCollisionShape after spawning.

--- Scenes and Scripts/Singletons/Building/BuildingManager.gd
+++ Scenes and Scripts/Singletons/Building/BuildingManager.gd
@@ -90,6 +90,7 @@ func init_building_system():
 			var instance = BA_PreSpawnedObject.instantiate()
 			PlayerManager.PLAYER.BuildingItemParent.add_child(instance)
 			instance.spawn(HandManager.CURRENTLY_HOLDING_ITEM)
+			PlayerManager.PLAYER.CanBuildCollisionShape.global_transform = instance.CollisionShape.global_transform
 			
 			print("spawned building system")
 
