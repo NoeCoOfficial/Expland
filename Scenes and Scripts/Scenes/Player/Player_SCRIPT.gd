e8af7ea50ba0cb8a2d053cd00ecb933540a89b48
sebashtioon <sebastiansuciu607@gmail.com>
2025-06-01 12:59:37 +1000

    fix: Update bone positions and rotations for player character and adjust scene transitions

--- Scenes and Scripts/Scenes/Player/Player_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Player/Player_SCRIPT.gd
@@ -1304,6 +1304,7 @@ func sleep_cycle(setSleeping : bool, incrementDay : bool, fadeInTime : float, ho
 		
 		on_sleep_cycle_hold_finished(fadeOutTime, time)
 	else:
+		await get_tree().create_timer(3.0).timeout
 		get_tree().change_scene_to_file("res://Scenes and Scripts/Scenes/Story Mode/Eryv Dream Cutscene/EryvDreamCutscene.tscn")
 
 
