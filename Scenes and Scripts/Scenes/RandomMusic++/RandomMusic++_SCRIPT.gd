75a4d1e33ece2173014d77fe43053a8cd8b772e8
sebashtioon <sebastiansuciu607@gmail.com>
2025-06-13 12:00:42 +1000

    bro whattt

--- Scenes and Scripts/Scenes/RandomMusic++/RandomMusic++_SCRIPT.gd
+++ Scenes and Scripts/Scenes/RandomMusic++/RandomMusic++_SCRIPT.gd
@@ -1,8 +1,11 @@
 class_name RandomMusic extends Node
 
 @export_group("Variables")
-@export var Streams : Array[AudioStream]
-@export var FadeTime : float = 2.0
+@export var Streams : Array[AudioStream] ## The streams you want to play.
+@export var FadeInTime : float = 2.0 ## How long it takes to fade in to a song.
+@export var FadeOutTime : float = 2.0 ## How long it takes to fade out a song.
+@export var LowerBoundRandomSec : float = 400.0 ## After a song finishes, a new song will start after at least this many seconds.
+@export var UpperBoundRandomSec : float = 1000.0 ## After a song finishes, a new song will start after at most this many seconds.
 
 @export_group("References")
 @export var RandomMusicPlayer : AudioStreamPlayer
