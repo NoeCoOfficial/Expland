d0aabe208e05c9e995d46589aea430c79cb0e77d
sebashtioon <sebastiansuciu607@gmail.com>
2025-06-23 18:12:04 +0300

    hide time label at endscreen if setting off also format time correctly

--- Scenes and Scripts/Scenes/The Island/TheIsland_SCRIPT.gd
+++ Scenes and Scripts/Scenes/The Island/TheIsland_SCRIPT.gd
@@ -161,7 +161,8 @@ func eryvDreamWakeUpDialogue():
 
 func spawnDemoScreen():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	$"Story Mode/Cameras/StoryModeDreamWakeUpCameraRig/StoryModeDreamWakeUpCamera/ExplandDemoNotice/Buttons/TimeLabel".text = str(TimeManager.speedrun_timer_get_time())
+	$"Story Mode/Cameras/StoryModeDreamWakeUpCameraRig/StoryModeDreamWakeUpCamera/ExplandDemoNotice/Buttons/TimeLabel".text = TimeManager.speedrun_timer_get_formatted_time()
+	$"Story Mode/Cameras/StoryModeDreamWakeUpCameraRig/StoryModeDreamWakeUpCamera/ExplandDemoNotice/Buttons/TimeLabel".visible = PlayerSettingsData.speedRunTimer
 	$"Story Mode/Cameras/StoryModeDreamWakeUpCameraRig/StoryModeDreamWakeUpCamera/ExplandDemoNotice/ExplandDemoUIAnimation".play("main")
 
 #####################################################
