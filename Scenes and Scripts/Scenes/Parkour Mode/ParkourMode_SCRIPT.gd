5de6ca6a19ab2a9c05f56e2c797e45e16320117d
sebashtioon <sebastiansuciu607@gmail.com>
2025-06-27 20:56:56 +0300

    ALMOST DONE!

--- Scenes and Scripts/Scenes/Parkour Mode/ParkourMode_SCRIPT.gd
+++ Scenes and Scripts/Scenes/Parkour Mode/ParkourMode_SCRIPT.gd
@@ -93,4 +93,3 @@ func _on_join_discord_server_button_pressed() -> void:
 func _on_respawn_box_body_entered(body: Node3D) -> void:
 	if body.is_in_group(&"PlayerBody"):
 		Player.global_position = Vector3(-0.11, 0.957, 2.595)
-		
